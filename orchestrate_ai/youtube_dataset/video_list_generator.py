86a2bc0d767daaf9e8bb4b201c5195ef3ece9add
Amrit Twanabasu <amrit.cas@gmail.com>
2016-12-29 18:07:39 +0545

    Add new playlists and update list generator to generate list of all emotional label videos

--- orchestrate_ai/youtube_dataset/video_list_generator.py
+++ orchestrate_ai/youtube_dataset/video_list_generator.py
@@ -23,31 +23,59 @@ class Generator:
 	"""Returns video list for given instrument and emotion
 	
 	"""
-	def get_video_list(self, instrument_name, emotion_label, force_refresh = False):
-		instrument_playlist_id = self.json_data["instruments"][instrument_name]["playlistId"]
-		emotion_playlist_id = self.json_data["emotions"][emotion_label]["playlistId"]
+	def get_video_list(self, instrument_name, emotion_label = None, force_refresh = False):
+		instrument_playlist_items = self.get_video_list_for_instrument(instrument_name, force_refresh)
+		emotion_playlist_items = self.get_video_list_for_emotion(emotion_label, force_refresh)
 
-		instrument_playlist_items, is_instrument_playlist_cached = self.get_cached_playlist_items(instrument_playlist_id)		
-		
-		emotion_playlist_items, is_emotion_playlist_cached = self.get_cached_playlist_items(emotion_playlist_id)		
+		video_list = list(set(instrument_playlist_items).intersection(emotion_playlist_items))
+
+		return video_list
+
+	"""Returns a video list for given instrument
+
+	"""
+	def get_video_list_for_instrument(self, instrument_name, force_refresh = False):
+		instrument_playlist_id = self.json_data["instruments"][instrument_name]["playlistId"]
+
+		instrument_playlist_items, is_instrument_playlist_cached = self.get_cached_playlist_items(instrument_playlist_id)				
 
 		if (not is_instrument_playlist_cached) or force_refresh:
 			print("Fetching Video list for playlist of instrument: '",instrument_name,"'")		
 			instrument_playlist_items = self.api_client.get_playlist_items(instrument_playlist_id)
 			self.cache_playlist_items(instrument_playlist_id, instrument_playlist_items)
 		else:
-			print("Using cached list for instrument: '",instrument_name,"'")		
+			print("Using cached list for instrument: '",instrument_name,"'")
 
-		if (not is_emotion_playlist_cached) or force_refresh:
-			print("Fetching Video list for playlist of emotion: '",emotion_label,"'")		
-			emotion_playlist_items = self.api_client.get_playlist_items(emotion_playlist_id)
-			self.cache_playlist_items(emotion_playlist_id, emotion_playlist_items)
+		return instrument_playlist_items
+
+	"""Returns a video list for given emotion
+
+	"""
+	def get_video_list_for_emotion(self, emotion_label = None, force_refresh = False):
+		emotions = self.json_data["emotions"]
+
+		if emotion_label is None:
+			# When no emotional label is provided it fetches all emotional lables from playlist
+			emotion_labels = [(emotion) for emotion in self.json_data["emotions"]]
 		else:
-			print("Using cached list for emotion: '",emotion_label,"'")
+			emotion_labels = [emotion_label]
 
-		video_list = list(set(instrument_playlist_items).intersection(emotion_playlist_items))
+		playlist_items = []
 
-		return video_list
+		for label in emotion_labels:
+			emotion_playlist_id = emotions[label]["playlistId"]
+			emotion_playlist_items, is_emotion_playlist_cached = self.get_cached_playlist_items(emotion_playlist_id)
+
+			if (not is_emotion_playlist_cached) or force_refresh:
+				print("Fetching Video list for playlist of emotion: '",label,"'")		
+				emotion_playlist_items = self.api_client.get_playlist_items(emotion_playlist_id)
+				self.cache_playlist_items(emotion_playlist_id, emotion_playlist_items)
+			else:
+				print("Using cached list for emotion: '",label,"'")
+
+			playlist_items = list(set().union(playlist_items,emotion_playlist_items))
+
+		return playlist_items
 
 	"""Returns cached playlist items
 	
--- prepare_dataset.py
+++ prepare_dataset.py
@@ -3,7 +3,7 @@ from orchestrate_ai.youtube_dataset import *
 def main():
 	# Download and prepare datasets
 	list_generator = video_list_generator.Generator("playlists.json", youtube_api_client.Client("AIzaSyD_UC-FpXbJeWzPfscLz9RhqSjKwj33q6A"))
-	video_list = list_generator.get_video_list("piano","sad")
+	video_list = list_generator.get_video_list("piano")
 	
 	downloader = youtube_video_downloader.Downloader()
 	downloader.download_from_list(video_list)
