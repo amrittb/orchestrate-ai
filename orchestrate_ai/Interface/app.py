f1c87893b3f504541ffa4981901c333c9c6b8d29
rsanjib <sanjibraj169@gmail.com>
2017-03-11 23:58:58 +0545

    add timestamp for file urls

--- orchestrate_ai/Interface/app.py
+++ orchestrate_ai/Interface/app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
 if __name__ == '__main__':
     app.run(debug=True)
