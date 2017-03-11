284b1cddb36ccf644f719221bf9773fadbeb228f
rsanjib <sanjibraj169@gmail.com>
2017-03-12 01:25:09 +0545

    creat lyric and midi file uploader

--- orchestrate_ai/Interface/app.py
+++ orchestrate_ai/Interface/app.py
@@ -1,10 +1,57 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request,  send_from_directory
+import os
+from werkzeug import secure_filename
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['ALLOWED_EXTENSIONS'] = set(['mid', 'txt'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Call python script to predict file
+        # return labels for each category
+        return render_template('result.html')
+
+@app.route('/upload_lyric', methods=['POST'])
+def upload_lyric():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Call python script to predict file
+        # return labels for each category
+        return render_template('result.html')
+
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
 
 def dated_url_for(endpoint, **values):
     if endpoint == 'static':
